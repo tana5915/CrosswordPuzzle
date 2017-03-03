78ccb4432c2be2d3879f80874fbd1f953980e3fb
Dave Dunn <dave.dunn@jointheleague.org>
2017-03-03 10:35:54 -0800

    update

--- src/CrosswordPuzzle.java
+++ src/CrosswordPuzzle.java
@@ -1,36 +1,5 @@
-import java.util.List;
-import java.util.ArrayList;
 
 public class CrosswordPuzzle
 {
-	private Square[][] puzzle;
 	
-	public CrosswordPuzzle( boolean[][] blackSquares ) 
-	{ 
-		//add code here for Part B of Question 3 	
-	} 
-    	
-	public boolean toBeLabeled( int r, int c, boolean[][] blackSquares )
-	{
-		//add code here for Part A of Question 3 	
-		return false;
-	}   
-    	
-    	//not part of the exam, but helpful to see the grid
-    public String toString()
-    {
-    	String ret = "";
-    	for( Square[] row : puzzle )
-    	{
-    		for( Square it : row )
-    		{
-    			if( it == null )
-    				ret += String.format("%-9s","null");
-    			else
-    				ret += it;
-    		}
-    		ret += "\n";
-    	}
-    	return ret;
-    }
 }
--- src/Square.java
+++ src/Square.java
@@ -1,26 +1,3 @@
-public class Square
-{
-	private boolean val;
-	private int number;
-	
-    public Square( boolean isBlack, int num ) 
-    {
-    	val = isBlack;
-    	number = num;
-    }  
-    	
-    public boolean getBlack()
-    {
-    	return val;
-    }
-    
-    public int getNumber()
-    {
-    	return number;
-    }
-        
-    public String toString()
-    {
-    	return String.format("%-9s",(val + " " + number));
-    }
+public class Square{
+
 }
