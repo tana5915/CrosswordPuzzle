8c50b58910876ed4fa89f6d82101dd84daea2641
Aidan Tan <league@WorthyMini-6.attlocal.net>
2017-03-25 12:24:30 -0700

    dsfdfsdsds
    sdff

--- src/CrosswordPuzzle.java
+++ src/CrosswordPuzzle.java
@@ -1,5 +1,36 @@
 
-public class CrosswordPuzzle
-{
-	
+public class CrosswordPuzzle {
+	private Square[][] puzzle;
+
+	public CrosswordPuzzle(boolean[][] blacksquares) {
+		puzzle = new Square[blacksquares.length][blacksquares[0].length];
+		int num = 1;
+		for (int r = 0; r < blacksquares.length; r++) {
+			for (int c = 0; c < blacksquares[0].length; c++) {
+				if (!toBeLabeled(r, c, blacksquares)) {
+					if (blacksquares[r][c]) {
+						puzzle[r][c] = new Square(true, 0);
+					}
+
+				} else if (toBeLabeled(r, c, blacksquares)) {
+					puzzle[r][c] = new Square(false, num);
+					num += 1;
+				} else {
+					puzzle[r][c] = new Square(false, 0);
+				}
+			}
+		}
+	}
+
+	private boolean toBeLabeled(int r, int c, boolean[][] blackSquares) {
+
+		if (!blackSquares[r][c]) {
+
+			if ((r == 0 || blackSquares[r - 1][c]) || (c == 0 || blackSquares[r][c - 1])) {
+				return true;
+			}
+		}
+
+		return false;
+	}
 }
--- src/Square.java
+++ src/Square.java
@@ -1,3 +1,9 @@
 public class Square{
-
+boolean isBlack;
+int num;
+public  Square(boolean isBlack, int num)
+{
+	this.isBlack= isBlack;
+	this.num=num;
+}
 }
